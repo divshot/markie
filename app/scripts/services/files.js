9cf428927e241f7f9a50b9d9db4af806dbb32c5e
Jake Johnson <jake@fastdivision.com>
2014-05-30 11:27:19 -0700

    Order files by modified timestamp and check if dirty when saving

--- app/scripts/services/files.js
+++ app/scripts/services/files.js
@@ -5,7 +5,11 @@ angular.module('markdownApp')
 .service('files', function($rootScope, $filter, localStorageService) {
   return {
     list: function() {
-      return localStorageService.get('files');
+      var files = _.sortBy(localStorageService.get('files'), function(file, id) {
+        file.id = id;
+        return -file.modified;
+      });
+      return files;
     },
     get: function(id) {
       return localStorageService.get('files')[id];
@@ -14,10 +18,14 @@ angular.module('markdownApp')
       var id = id || $filter('filename')(data.title);
       var files = localStorageService.get('files') || {};
 
+      if(files[id].title === data.title && files[id].content === data.content)
+        return false;
+
       if(!files[id]) files[id] = {}
 
       files[id].title = data.title;
       files[id].content = data.content;
+      files[id].modified = Date.now();
 
       localStorageService.set('files', JSON.stringify(files));
       $rootScope.$broadcast('file:update');
